1dfb1941cdb9d81a8ba0b6e5020c6bdef26a73c4
OH-JOO-YEONG <brb1111@naver.com>
2025-05-24 02:43:31 +0900

    [Feat] 사진 조회 페이지 정보 영역 스켈레톤 처리

--- apps/web/app/(main)/result/components/info-card.tsx
+++ apps/web/app/(main)/result/components/info-card.tsx
@@ -5,21 +5,35 @@ interface InfoCardProps {
   label: string;
   value: string | number;
   isLink?: boolean;
+  isLoading?: boolean;
 }
 
-const InfoCard = ({ label, value, isLink = false }: InfoCardProps) => {
+const InfoCard = ({
+  label,
+  value,
+  isLink = false,
+  isLoading = false,
+}: InfoCardProps) => {
   return (
     <div className={styles.infoCard}>
       <div className={styles.label}>{label}</div>
-      <div className={styles.value}>
-        {isLink ? (
-          <a href={value.toString()} target="_blank" rel="noopener noreferrer">
-            {value}
-          </a>
-        ) : (
-          value
-        )}
-      </div>
+      {isLoading ? (
+        <div className={styles.skeleton}></div>
+      ) : (
+        <div className={styles.value}>
+          {isLink ? (
+            <a
+              href={value.toString()}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {value}
+            </a>
+          ) : (
+            value
+          )}
+        </div>
+      )}
     </div>
   );
 };
